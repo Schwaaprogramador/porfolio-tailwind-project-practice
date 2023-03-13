56e28a6d8503e23bdac35177bd429ccc22bbba78
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-03-12 20:30:13 -0500

    Main structure, components emptys

--- portfolio-3d/src/App.jsx
+++ portfolio-3d/src/App.jsx
@@ -1,12 +1,33 @@
+import { BrowserRouter } from 'react-router-dom';
+import {About, Contact, Experience, Feedbacks, 
+  Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 
 function App() {
   
 
   return (
-    <div>
-      3d
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bf-primary'>
+        <div className='bg-her-pattern bg-cover bg-no-repeat bg center'>
+          <Navbar/>
+          <Hero/>
+        </div>
+
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+
+        <div className='relative z-0'>
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+
+      </div>
+    
+    </BrowserRouter>
   )
 }
 
